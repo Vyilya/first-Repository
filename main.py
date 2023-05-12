97ecbff69158442fa5922b0642e65bd1a1c569ef
Vyilya <goru89@mail.ru>
2023-05-12 23:35:42 +0400

    Добавил переменные с урока

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ print (x*6)
 print (x*7)
 print (x*8)
 print (x*9)
+
+type_int = 1 # целые числа (int)
+type_float = 1.23 # числа с точкой
+type_str = "Строки пишутся в кавычках"
+type_bool_one = True # булевы значения истина (правда)
+type_bool_two = False # булевы значения ложь
+type_none = None
