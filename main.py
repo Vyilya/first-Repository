1102d0df686c9b18e60881e8df36143acd5f4eae
MKA <MKA@ULTOP.local>
2023-05-16 21:22:06 +0400

    урок

--- main.py
+++ main.py
@@ -1,17 +1,79 @@
-print ("Hello word")
-x = 5
-print (x*2)
-print (x*3)
-print (x*4)
-print (x*5)
-print (x*6)
-print (x*7)
-print (x*8)
-print (x*9)
+# print ("Hello word")
+# x = 5
+# print (x*2)
+# print (x*3)
+# print (x*4)
+# print (x*5)
+# print (x*6)
+# print (x*7)
+# print (x*8)
+# print (x*9)
 
-type_int = 1 # целые числа (int)
-type_float = 1.23 # числа с точкой
-type_str = "Строки пишутся в кавычках"
-type_bool_one = True # булевы значения истина (правда)
-type_bool_two = False # булевы значения ложь
-type_none = None
+# type_int = 1 # целые числа (int)
+# type_float = 1.23 # числа с точкой
+# type_str = "Строки пишутся в кавычках"
+# type_bool_one = True # булевы значения истина (правда)
+# type_bool_two = False # булевы значения ложь
+# type_none = None
+
+# a = 123 #int
+# print (a * 2, type(a)) #int
+# a = str(a) # str
+# print (a * 2, type(a)) #str
+# a = float(f) # float
+# print (a * 2, type(a)) #float
+
+# b = "123"
+# print (b, type(b)) 
+# b = int (b)
+# print (b, type(b))
+# b = 2
+
+# c = int(input("Мы умножим ваше число на два")) # input по умолчанию строковый с int числовым
+# print(c* 2)
+
+# print("Заполните Информацию")
+# myName = input("Введите имя: ")
+# firstName = input("Введите фамилию: ")
+# Age = int(input("Укажите возраст: "))
+# Jop = input ("Место работы: ")
+
+# print("Информация о пользователе")
+# print("Имя: ", myName)
+# print("Фамилия: ", firstName)
+# print("Возраст :", Age )
+# print("Работа: ", Jop)
+
+# myName = "Илья"
+# print("Привет" + " " + myName) # Конкатенация складываем строки
+# print(f"Привет {myName}") # шаблон строки
+# print("Привет"," ","мир!")
+
+# x = int(input("введите число: "))
+# print(x // 10) # // - деление до целого
+# print(x % 10) # % - остаток
+
+# a = 2
+# b = 3
+# h = a / b * 10
+# print(int(h))
+# print(type(h))
+
+# x = int(input("введите число: "))
+
+# x1 = (x % 10)
+# x = x // 10
+# x2 = (x % 10)
+# x = x // 10
+# x3 = (x % 10)
+# print(x1)
+# print(x2)
+# print(x3)
+# sum = (x1+x2+x3)
+# print(sum)
+
+# x = int(input("введите число: "))
+# z = int(input("введите число: "))
+# y = str(x)+str(z)
+# y = int (y)
+# print (y)
