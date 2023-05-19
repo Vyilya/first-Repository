accf055a1da87060a2cacbd0ff0057130dc045be
MKA <MKA@ULTOP.local>
2023-05-19 21:08:35 +0400

    урок 19.05

---
+++ TOP/main.py
@@ -0,0 +1,187 @@
+# print ("Hello word")
+# x = 5
+# print (x*2)
+# print (x*3)
+# print (x*4)
+# print (x*5)
+# print (x*6)
+# print (x*7)
+# print (x*8)
+# print (x*9)
+
+# type_int = 1 # целые числа (int)
+# type_float = 1.23 # числа с точкой
+# type_str = "Строки пишутся в кавычках"
+# type_bool_one = True # булевы значения истина (правда)
+# type_bool_two = False # булевы значения ложь
+# type_none = None
+
+# a = 123 #int
+# print (a * 2, type(a)) #int
+# a = str(a) # str
+# print (a * 2, type(a)) #str
+# a = float(f) # float
+# print (a * 2, type(a)) #float
+
+# b = "123"
+# print (b, type(b)) 
+# b = int (b)
+# print (b, type(b))
+# b = 2
+
+# c = int(input("Мы умножим ваше число на два")) # input по умолчанию строковый с int числовым
+# print(c* 2)
+
+# print("Заполните Информацию")
+# myName = input("Введите имя: ")
+# firstName = input("Введите фамилию: ")
+# Age = int(input("Укажите возраст: "))
+# Jop = input ("Место работы: ")
+
+# print("Информация о пользователе")
+# print("Имя: ", myName)
+# print("Фамилия: ", firstName)
+# print("Возраст :", Age )
+# print("Работа: ", Jop)
+
+# myName = "Илья"
+# print("Привет" + " " + myName) # Конкатенация складываем строки
+# print(f"Привет {myName}") # шаблон строки
+# print("Привет"," ","мир!")
+
+# x = int(input("введите число: "))
+# print(x // 10) # // - деление до целого
+# print(x % 10) # % - остаток
+
+# a = 2
+# b = 3
+# h = a / b * 10
+# print(int(h))
+# print(type(h))
+
+# x = int(input("введите число: "))
+
+# x1 = (x % 10)
+# x = x // 10
+# x2 = (x % 10)
+# x = x // 10
+# x3 = (x % 10)
+# print(x1)
+# print(x2)
+# print(x3)
+# sum = (x1+x2+x3)
+# print(sum)
+
+# x = int(input("введите число: "))
+# z = int(input("введите число: "))
+# y = str(x)+str(z)
+# y = int (y)
+# print (y)
+
+# a = 5
+# b = 10
+# print(a>b)
+# print(a<b)
+
+# a = 5
+# b = 10
+# if (a < b and b > 10) or a == 5: #True
+#     print("if выполнился")
+# # if a > b: #False
+# #     print("if 2 выполнился")
+# else:
+#     print("else выполнился")
+    
+    
+# a = 5
+# b = 10
+# if a > b:
+#     print("if выполнился") #True
+# elif a < b:
+#     print("elif выполнился")
+# else:
+#     print("else выполнился")
+
+    
+# login = input("Ведите логин: ")
+# password = input("Ведите пароль: ")
+# if login == "admin":
+#     if password == "admin":
+#         print("Вход выполнен")
+
+#     else:
+#         print("Не верный логин или пароль : 2 условие")
+#         # help = input("Сообщение для тех поддержки")
+# else:
+#     print("Не верный логин или пароль : 1 условие")
+
+# q1 = input("Зимой и летом одним цветом: ")
+# score = 0
+# if q1 == "ёлка" or q1 == "елка":
+#     print("Ответ верный")
+#     score = score + 1
+# else:
+#     print("Ответ не верный")
+    
+# q2 = input("Висит груша нельзя скушать: ")
+# if q2 == "лампочка":
+#     print("Ответ верный")
+#     score = score + 1
+# else:
+#     print("Ответ не верный")
+    
+# q3 = input("Что выше леса поднимается да без огня горит: ")
+# if q3 == "солнце":
+#     print("Ответ верный")
+#     score = score + 1
+# else:
+#     print("Ответ не верный")
+
+# q4 = input("Без рук, без ног, А ворота открывает: ")
+# if q4 == "ветер":
+#     print("Ответ верный")
+#     score = score + 1
+# else:
+#     print("Ответ не верный")
+# q5 = input("На дворе горой, а в избе водой: ")
+# if q5 == "снег":
+#     print("Ответ верный")
+#     score = score + 1
+# else:
+#     print("Ответ не верный")
+# print("У Вас очков: " , score)
+
+# if score >= 3:
+#     x = input("Хотите ответить на доп. вопрос? ")
+#     if x == ("да" or "yes"):
+#        y = input("Ах, не трогайте меня!Обожгу и без огня: ")
+#        if y == "крапива":
+#         print("Ответ верный, игра окончена")
+#        else:
+#         print("Игра Окончена, все очки сгорели")             
+#     else:
+#         print("Игра Окончена")
+
+# != - не равно
+
+# x = int(input("введите число: "))
+# if x % 2 == 0:
+#     print("четное")
+# else:
+#     print("неченое")
+
+# x = int(input("введите число: "))
+# y = int(input("введите число: "))
+# s = int(input("0 - сумма, 1 - раздница, 2 - средняя, 3 - произведение: "))
+# if s == 0:
+#     print(x + y)
+# elif s == 1:
+#     print(x - y)
+# elif s == 2:
+#     print(x + y / 2)
+# elif s == 3:
+#     print(x * y)
+# else:
+#     print("введите корректное значение")
+
+
