4b4487bfd9ac75ccd5bef1d4d7b63b5ceb0fd1e9
Vyilya <goru89@mail.ru>
2023-05-22 11:10:36 +0400

    Добавил дз1 и дз2

---
+++ HOME/Dz1/main.py
@@ -0,0 +1,30 @@
+# Задание 1
+# x = int(input("введите число: "))
+# y = int(input("введите число: "))
+# s = int(input("введите число: "))
+# print(x + y + s)
+# print(x * y * s)
+
+
+# Задание 2
+# x = int(input("введите зп за месяц: "))
+# y = int(input("введите сумму месяцного платежа по кредиту: "))
+# z = int(input("введите задолженность за комунальные услуги: "))
+# print(x-y-z,"остаток")
+
+# Задание 3
+# x = int(input("введите длину диагонали: "))
+# y = int(input("введите длину диагонали: "))
+# sum = int((x*y) / 2)
+# print("площадь ромба ровна:", sum)
+
+# Задание 4
+# print("To be")
+# print("or not")
+# print("to be")
+
+# Задание 5
+# print('"' + "Life is what happens")
+# print("    when")
+# print("        you're busy making other plans" + '"')
+# print("                                     John Lennon.")
