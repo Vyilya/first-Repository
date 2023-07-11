cb6b56c4a24ee8fc7583ced803872a77c80b3390
MKA <MKA@ULTOP.local>
2023-07-11 21:18:07 +0400

    oop

--- TOP/main.py
+++ TOP/main.py
@@ -396,6 +396,7 @@ class User():
     # Если в классе есть методы с словом update, значит этот метод для изменения информации
     def update_first_name(self, new_first_name):
         self.first_name = new_first_name
+        print(f"новое имя: {self.first_name}")
     def update_last_name(self, new_last_name):
         self.last_name = new_last_name
     def update_birthday(self, new_birthday):
@@ -412,31 +413,31 @@ class Moderator(User):
         self.status = "moderator"
 
         # Блокировка пользователей.
-        def blocking_user(self, users_list):
-            text_user_list = f"id  |  first_name  |  blocking  |  status \n"
-            for i in range(0, len(users_list)):
-                text_user_list += f" - {users_list[i].user_id}:{users_list[i].first_name} - {users_list[i].blocking} {users_list[i].status}\n"
-            print(text_user_list)
-            input_user_id = int(input("Введите id пользователя для блокировки: "))
-            for i in range(0, len(users_list)):
-                if self.status == "moderator":
-                    if input_user_id == i and users_list[i]['status'] != "moderator" and users_list[i]['status'] != "admin":
-                        if users_list[i]['blocking'] == True:
-                            print("Пользователь уже заблокирован")
-                            break
-                        else:
-                            users_list[i]['blocking'] = True
-                            print("Пользователь успешно заблокирован")
-                            break
-                elif self.status == "admin":
-                    if input_user_id == i:
-                        if users_list[i]['blocking'] == True:
-                            print("Пользователь уже заблокирован")
-                            break
-                        else:
-                            users_list[i]['blocking'] = True
-                            print("Пользователь успешно заблокирован")
-                            break
+    def blocking_user(self, users_list):
+        text_user_list = f"id  |  first_name  |  blocking  |  status \n"
+        for i in range(0, len(users_list)):
+            text_user_list += f" - {users_list[i].user_id}:{users_list[i].first_name} - {users_list[i].blocking} {users_list[i].status}\n"
+        print(text_user_list)
+        input_user_id = int(input("Введите id пользователя для блокировки: "))
+        for i in range(0, len(users_list)):
+            if self.status == "moderator":
+                if input_user_id == i and users_list[i]['status'] != "moderator" and users_list[i]['status'] != "admin":
+                    if users_list[i]['blocking'] == True:
+                        print("Пользователь уже заблокирован")
+                        break
+                    else:
+                        users_list[i]['blocking'] = True
+                        print("Пользователь успешно заблокирован")
+                        break
+            elif self.status == "admin":
+                if input_user_id == i:
+                    if users_list[i]['blocking'] == True:
+                        print("Пользователь уже заблокирован")
+                        break
+                    else:
+                        users_list[i]['blocking'] = True
+                        print("Пользователь успешно заблокирован")
+                        break
 
 
 class Admin(Moderator):
@@ -449,6 +450,7 @@ class Admin(Moderator):
         print("База данных пуста")
                                 # massiv  - массивом данных
     def create_users_list(self, massiv, users_list):
+        print(len(users_list))
         for i in range(0, len(massiv)):
             users_list.append(User(user_id=i, 
                                 first_name=massiv[i]["first_name"], 
@@ -458,6 +460,82 @@ class Admin(Moderator):
                                 login=massiv[i]["login"],
                                 password=massiv[i]["password"]))
 
-myAdmin = Admin(10, "admin" , "admin", "01.01.1970", "Мужской", "admin", "admin")
-myAdmin.create_users_list(base_list, registered_users)
-myAdmin.blocking_user(registered_users)
+class Registration():
+    def __init__(self) -> None:
+        pass
+    def create_user(self, users_list):
+        users_list.append(User( len(users_list), input("введите имя: "), 
+                                                            input("введите фамилию: "), 
+                                                            input("Дата рождения(дд.мм.гггг): "), 
+                                                            input("введите пол: "),  
+                                                            input("придумайте логин: "), 
+                                                            input("придумайте пароль: ")))
+class InLog():
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+    def log_in_account(self, users_list):
+        for i in range(len(users_list)):
+            if users_list[i].login == self.login and users_list[i].password == self.password:
+                print("вход выполнен")
+                break
+            elif i == len(users_list):
+                print("Ошибка")
+
+class Manager():
+    def __init__(self, reg,inlog,userModerAdmin,datalist):
+        self.reg = reg
+        self.inlog = inlog
+        self.userModerAdmin = userModerAdmin
+        self.datalist = datalist
+    def work(self):
+        print(self)
+
+my_reg = Registration
+my_inLog = InLog
+my_person_list = [User,Moderator,Admin]
+my_base = []
+# my_person_list[0](10, "admin" , "admin", "01.01.1970", "Мужской", "admin", "admin")
+myManager = Manager(my_reg,my_inLog,my_person_list,my_base)
+myManager.userModerAdmin[0](10, "admin" , "admin", "01.01.1970", "Мужской", "admin", "admin")
+myManager.inlog("admin", "admin")
+
+
+
+
+
+# proverka = Registration()
+# proverka.create_user()
+
+# myAdmin = Admin(10, "admin" , "admin", "01.01.1970", "Мужской", "admin", "admin")
+# myAdmin.create_users_list(base_list, registered_users)
+
+# myLogin = InLog(input("Введите логин: "), input("Введите пароль: "))
+# myLogin.log_in_account()
+
+# myAdmin.blocking_user(registered_users)
+# print(registered_users[0].update_first_name(input("введите новое имя: ")))
+# print(registered_users[0].first_name)
+
+# base_list = [
+#     {
+#         "first_name" : "Илья",
+#         "last_name" : "Выборнов",
+#         "birthday" : "19.11.1989",
+#         "gender" : "Мужской",
+#         "login" : "Vyilya",
+#         "password" : "admin",
+#     },
+#     {
+#         "first_name" : "Виталий",
+#         "last_name" : "Васин",
+#         "birthday" : "25.01.1999",
+#         "gender" : "Мужской",
+#         "login" : "Vasa",
+#         "password" : "12345" ,  
+#     },]
+
+# primer = [
+#     User(0,"name","last_name","01.01.2000", "Мужской","login","pass"),
+#     User(1,"name","last_name","01.01.2000", "Мужской","login","pass"),
+# ]
