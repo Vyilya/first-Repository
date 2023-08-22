614256840d91b1dee772c4da1b872b470b271153
MKA <MKA@ULTOP.local>
2023-08-22 21:09:58 +0400

    django

---
+++ django/pythonProject/index/views.py
@@ -0,0 +1,22 @@
+import random
+
+from django.shortcuts import render
+
+# Create your views here.
+def indexView(request):
+    return render(request, 'index.html')
+
+def passwordView(request):
+    newpassword = ''
+    length = int(request.GET.get('length'))
+    text = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('upper'):
+        text.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('numbers'):
+        text.extend('0123456879')
+    if request.GET.get('symbols'):
+        text.extend('!"№;%:?*$#')
+    for i in range(length):
+        newpassword += text[random.randint(0,len(text)-1)]
+    #                                           из html       из py
+    return render(request, 'password.html', {'newpassword':newpassword})
