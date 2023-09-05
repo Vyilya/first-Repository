ff0c5c154690e6ad93ce4ca6da9f6bd4f1a51b59
MKA <MKA@ULTOP.local>
2023-09-05 20:59:59 +0400

    style

--- django/pythonProject1/mysite/settings.py
+++ django/pythonProject1/mysite/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
